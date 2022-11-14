f89b954a69ddb1e012dee9cdbc23d6d2190cc23e
Ludwig Pfann <70544031+Paranemi@users.noreply.github.com>
2022-11-14 15:49:05 +0100

    Add project files.

---
+++ BlazorAgGridTest/Data/PeopleModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorAgGridTest.Data;
+
+public class PeopleModel
+{
+    [JsonProperty("_id")]
+    public string Id { get; set; }
+
+    [JsonProperty("age")]
+    public int Age { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [JsonProperty("address")]
+    public string Address { get; set; }
+
+    [JsonProperty("registered")]
+    public DateTime Registered { get; set; }
+}
---
+++ BlazorAgGridTest/wwwroot/js/JavaScript.js
@@ -0,0 +1,21 @@
+﻿const columnDefs = [];
+
+function initGridColumns(fieldName, title, width, editable, sortable) {
+
+    columnDefs.push({ field: fieldName, headerName: title, width: width, sortable: sortable, editable: editable, })
+}
+
+function initAgGrid(json) {
+    const forecasts = JSON.parse(json);
+
+    const gridOptions = {
+        columnDefs: columnDefs,
+        rowData: forecasts
+    };
+
+    // setup the grid after the page has finished loading
+    const gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+}
+
+
