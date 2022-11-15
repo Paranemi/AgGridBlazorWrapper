d8564ee1c8c27ba89c06004281156ab6df882eda
Ludwig Pfann <70544031+Paranemi@users.noreply.github.com>
2022-11-15 14:00:13 +0100

    enable cell editing in agGrid blazor component

--- BlazorAgGridTest/wwwroot/js/JavaScript.js
+++ BlazorAgGridTest/wwwroot/js/JavaScript.js
@@ -1,26 +1,51 @@
 ﻿const columnDefs = [];
 let rowData = [];
+let rowImmutableStore;
+let gridOptions;
+let _dotNetInstance;
 
 function initGridColumns(field, title, width, resizable, editable, sortable) {
     columnDefs.push({ field: field, headerName: title, resizable: resizable, editable: editable, sortable: sortable, });
 }
 
-function initAgGridData(json, pageable, pageSize) {
+function initAgGridRowData(json, pageable, pageSize, dotNetInstance) {
+    _dotNetInstance = dotNetInstance;
     rowData = JSON.parse(json);
     setTimeout(() => initAgGrid(pageable, pageSize));
 }
 
 function initAgGrid(pageable, pageSize) {
-    const gridOptions = {
+    gridOptions={
         columnDefs: columnDefs,
         rowData: rowData,
         pagination: pageable,
         paginationPageSize: pageSize,
         rowDragManaged: true,
         animateRows: true,
+        //getRowId: (params) => params.data.id,
+        readOnlyEdit: true,
+        onCellEditRequest: onEdit,
     };
 
     // setup the grid after the page has finished loading
     const gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
+}
+
+function onEdit(event) {
+    const data = event.data;
+    const field = event.colDef.field;
+    const newValue = event.newValue;
+    const newItem = { ...data };
+    newItem[field] = event.newValue;
+
+    console.log('onCellEditRequest, updating ' + field + ' to ' + newValue);
+
+    rowData = rowData.map((oldItem) =>
+        oldItem.Id == newItem.Id ? newItem : oldItem
+    );
+
+    gridOptions.api.setRowData(rowData);
+
+    _dotNetInstance.invokeMethodAsync('CellEdited', JSON.stringify(newItem));
 }
